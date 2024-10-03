175711a01a04330e2ac516f2f2dfbb6ebb3f5e69
Rex6961 <aon777ar@gmail.com>
2024-10-03 15:47:36 -0300

    Created models of DB and low and high levels of view and template of applications board'

---
+++ elecbullboard/bboard/views.py
@@ -0,0 +1,20 @@
+# for low level
+from django.http import HttpResponse
+from django.template import loader
+
+# for high level
+from django.shortcuts import render
+
+from .models import Bb
+
+# low level access
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by("-published")
+#     context = {'bbs': bbs}
+#     return HttpResponse(template.render(context, request))
+
+# high level access
+def index(request):
+    bbs = Bb.objects.order_by("-published")
+    return render(request=request, template_name='bboard/index.html', context={'bbs': bbs})
